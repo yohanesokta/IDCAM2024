c584acbbec325382a63bbd9df2025d81e747b2fb
yohanesokta <yohanesoktayt72@gmail.com>
2024-09-17 07:23:23 +0700

    update: pertemuan 2

---
+++ pertemuan2/bin/pertemuan2.dart
@@ -0,0 +1,8 @@
+import 'package:pertemuan2/pertemuan2.dart' as pertemuan2;
+
+void main(List<String> arguments) {
+  var data = ["Yohanes","Noval","Chellin","Winarty","Wolyo","Ashari"];
+  for(var i = 0; i < data.length; i++){
+    print(data[i]);
+  }
+}
---
+++ pertemuan2/test/pertemuan2_test.dart
@@ -0,0 +1,8 @@
+import 'package:pertemuan2/pertemuan2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

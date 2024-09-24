ccb1a9fe7f4a665fdf6b1b1515d1566da5de8878
yohanesokta <yohanesoktayt72@gmail.com>
2024-09-24 23:05:44 +0700

    pertemuan next

--- pertemuan4/bin/pertemuan4.dart
+++ pertemuan4/bin/pertemuan4.dart
@@ -1,8 +1,14 @@
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
-  var yohamCat = pertemuan4.Animal("Hanz",4,4.2);
-  yohamCat.eat();
-  yohamCat.poop();
-  print(yohamCat.weight);
+  // var yohamCat = pertemuan4.Animal("Hanz",4,4.2);
+  // yohamCat.eat();
+  // yohamCat.poop();
+  // print(yohamCat.weight);
+  // print(yohamCat.age);
+
+  var litleAnimal = pertemuan4.AninalConstructor("Little", 4,4.2)
+  ..named();
+
+// casacade notation
 }
--- pertemuan4/lib/pertemuan4.dart
+++ pertemuan4/lib/pertemuan4.dart
@@ -3,15 +3,21 @@ int calculate() {
 }
 
 class Animal {
-  String name;
-  int age;
-  double weight;
+  String _name;
+  int _age;
+  double _weight;
 
-  Animal(this.name,this.age,this.weight);
-
+  Animal(this._name,this._age,this._weight);
+  // setter
+  set name(String value){
+    _name = value;
+  }
+  // getter
+  double get weight => _weight;
+  int get age => _age;
   void eat() {
       print("Eating");
-      weight += 0.2;
+      _weight += 0.2;
   }
 
   void sleep() {
@@ -19,6 +25,23 @@ class Animal {
   }
   void poop(){
     print('popping');
-    weight -= 0.1;
+    _weight -= 0.1;
   }
+}
+
+class AninalConstructor{
+  String name ="";
+  int age = 0;
+  double weight = 0;
+
+  AninalConstructor(String name, int age, double weight){
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
+  }
+  void named() {
+
+    print("Hello " + name);
+  }
+
 }
